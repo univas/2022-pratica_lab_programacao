780ac8a0a15e6abd2c03cb99bb1e3abbe8220f8e
Your Name <you@example.com>
2022-09-14 21:42:04 -0300

    Add sample of FOR inside another FOR

---
+++ aula-07/src/br/edu/univas/StartApp.java
@@ -0,0 +1,14 @@
+package br.edu.univas;
+
+public class StartApp {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println("");
+        }
+    }
+}
